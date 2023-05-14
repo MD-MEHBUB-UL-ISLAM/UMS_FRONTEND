b55701b00e9271e3f9988c5147663644a3bc98f4
MD MEHBUB UL ISLAM <114100350+MD-MEHBUB-UL-ISLAM@users.noreply.github.com>
2023-05-14 21:34:17 +0600

    deploy 1.6

--- pages/studentreg/dashboard/getbook.js
+++ pages/studentreg/dashboard/getbook.js
@@ -36,7 +36,7 @@ export default function GetUsers({ data }) {
   export async function getServerSideProps(context) {
     const id = context.params.id;
   
-    const response = await axios.get('https://umsbackendteo-production.up.railway.ap/reg/findbook/' + id);
+    const response = await axios.get('https://umsbackendteo-production.up.railway.app/reg/findbook/' + id);
     const data = await response.data;
   
     return { props: { data } }
