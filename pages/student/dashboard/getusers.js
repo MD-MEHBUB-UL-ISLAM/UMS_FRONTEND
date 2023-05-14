1f51e4a69005a652cd19a49c4a3a8ce21754e2a0
MD MEHBUB UL ISLAM <114100350+MD-MEHBUB-UL-ISLAM@users.noreply.github.com>
2023-05-14 14:40:27 +0600

    deploy 1.3

--- pages/student/dashboard/getusers.js
+++ pages/student/dashboard/getusers.js
@@ -1,22 +1,37 @@
-import Link from "next/link"
-import MyLayout from "../../component/layout";
-import axios from "axios";
 
+
+  import Link from "next/link"
+
+import axios from "axios";
+import { useRouter } from 'next/router'
+
+import SessionCheck from "@/pages/component/sessioncheck";
+import MyLayout from "@/pages/component/layout";
+import AdminDrawer from "@/pages/component/admindrawer";
+import Footer from "@/pages/component/footer";
 
 export default function GetUsers({ data }) {
-    
+  const router = useRouter();
   return (
-      <>
-       <MyLayout title="Get Users"/>
-      <h1>All Users</h1>
+    <>
+      <SessionCheck />
+      <MyLayout title="Register ID" />
+      <AdminDrawer />
+      <div class="p-24 sm:ml-64">
+        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        All Users
+              </h1>
       <ul>
         {data.map(item => (
           <li key={item.id}>
       
-        <Link href={"/student/dashboard/users/"+item.id}>{item.fullname}</Link>
+            <Link href={"/student/dashboard/users/" + item.id}><div className="font-bold text-blue-600/100 bg-gray-200 px-16 m-10"
+            >{item.fullname}</div></Link>
             </li>
         ))}
       </ul>
+        </div>
+       <Footer/> 
     </>
   );
   }
@@ -28,3 +43,4 @@ export default function GetUsers({ data }) {
     
   return { props: { data } }
   }
+  
