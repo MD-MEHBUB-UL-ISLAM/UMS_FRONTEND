eea9cd124dec0c59002d8d3d01c3d76fa2ed9eda
MD MEHBUB UL ISLAM <114100350+MD-MEHBUB-UL-ISLAM@users.noreply.github.com>
2023-05-15 00:12:13 +0600

    deploy 1.32

--- pages/studentreg/dashboard/users/[id].js
+++ pages/studentreg/dashboard/users/[id].js
@@ -1,32 +1,52 @@
-import MyLayout from "../../../component/layout";
-import Image from "next/image";
+
+
+
+
+
+
+
+import MyLayout from "@/pages/component/layout";
+import MyLayout1 from "@/pages/component/layout1";
 import axios from "axios";
 import { useRouter } from 'next/router'
 
-export default function UserProfile({ data } ) {
-const router = useRouter();
-    return (
-      <>
-      <MyLayout title="User"/>
-   
+import UserLayout from "@/pages/component/userdata";
+import UserLayout1 from "@/pages/component/userdata2";
+import SessionCheck from "../../../component/sessioncheck";
+import SessionCheck1 from "@/pages/component/sessioncheck1";
+import AdminDrawer from "../../../component/admindrawer";
+import AdminDrawer1 from "@/pages/component/admindrawer1";
 
-      <h1>Name: {data.name}</h1>
-      <h1>Email: {data.email}</h1>
-      <h1>Address: {data.address}</h1>
-    
-      <button type="button" onClick={() => router.back()}>
-      Click here to go back
-    </button>
+export default function UserProfile({ data }) {
+  const router = useRouter();
+  return (
+    <>
+      <SessionCheck1 />
+      <MyLayout1 title={data.id} />
+      <AdminDrawer1 />
+
+      <UserLayout1
+        filename={data.filename}
      
-      </>
-    )
-  }
-  
- export async function getServerSideProps(context) {
- const id=context.params.id;
+        Name= {data.name}
+        Email= {data.email}
+        Password= {data.password}
+        address= {data.address}
+      />
 
-    const response = await axios.get('https://umsbackendteo-production.up.railway.app/reg/findadmin/'+id);
-    const data = await response.data;
-   
-return { props: { data } }
+      <button type="button" onClick={() => router.back()}>
+        Click here to go back
+      </button>
+
+    </>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+
+  const response = await axios.get('https://umsbackendteo-production.up.railway.app/reg/findadmin/' + id);
+  const data = await response.data;
+
+  return { props: { data } }
 }
