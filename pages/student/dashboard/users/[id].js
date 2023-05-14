86d6ee2e06ad1399edb92074f83f2298f488b5d0
MD MEHBUB UL ISLAM <114100350+MD-MEHBUB-UL-ISLAM@users.noreply.github.com>
2023-05-14 22:51:55 +0600

    deploy 1.23

--- pages/student/dashboard/users/[id].js
+++ pages/student/dashboard/users/[id].js
@@ -1,38 +1,47 @@
 
-import Image from "next/image";
+
+
+import MyLayout from "@/pages/component/layout";
 import axios from "axios";
-import { useRouter } from 'next/router';
-import MyLayout from "../../../component/layout";
+import { useRouter } from 'next/router'
 
-export default function UserProfile({ data } ) {
-const router = useRouter();
-    return (
-      <>
-      <MyLayout title="User"/>
-   
-      <h1>Name: {data.fullname}</h1>
-  <h1>CGPA: {data.CGPA}</h1>
-  <h1>phone: {data. phone}</h1>
-  <h1>isActive: {data.isActive}</h1>
-  <h1>Email: {data.email}</h1>
-  <h1>Password: {data.password}</h1>
-  <h1> Info: {data.info}</h1>
-      <h1>Picture: </h1>
-      <Image src={"https://umsbackendteo-production.up.railway.app/student/getimage/"+data.filename} alt="me" width="150" height="150" />
-     <br></br>
-      <button type="button" onClick={() => router.back()}>
-      Click here to go back
-    </button>
+import UserLayout from "@/pages/component/userdata";
+import SessionCheck from "../../../component/sessioncheck";
+import AdminDrawer from "../../../component/admindrawer";
+
+export default function UserProfile({ data }) {
+  const router = useRouter();
+  return (
+    <>
+      <SessionCheck />
+      <MyLayout title={data.id} />
+      <AdminDrawer />
+
+      <UserLayout
+        filename={data.filename}
      
-      </>
-    )
-  }
-  
- export async function getServerSideProps(context) {
- const id=context.params.id;
+        Name= {data.fullname}
+        CGPA= {data.CGPA}
+        phone= {data. phone}
+        isActive= {data.isActive}
+        Email= {data.email}
+        Password= {data.password}
+        Info= {data.info}
+      />
 
-    const response = await axios.get('https://umsbackendteo-production.up.railway.app/student/findstudent/'+id);
-    const data = await response.data;
-   
-return { props: { data } }
+      <button type="button" onClick={() => router.back()}>
+        Click here to go back
+      </button>
+
+    </>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const id = context.params.id;
+
+  const response = await axios.get('https://umsbackendteo-production.up.railway.app/student/findstudent/' + id);
+  const data = await response.data;
+
+  return { props: { data } }
 }
